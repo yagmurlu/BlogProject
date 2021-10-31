76b0babc0bc215d0215beb66a378534eb9e6d55e
yagmurlu <mavibulut0106@gmail.com>
2021-10-31 14:45:47 +0300

    View Component with Comment Created

---
+++ BlogProject/ViewComponents/Comment/CommentByListBlog.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.ViewComponents.Comment
+{
+    public class CommentByListBlog:ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList(2);
+            return View(values);
+        }
+
+    }
+}
