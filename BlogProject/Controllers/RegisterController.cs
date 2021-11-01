ad0df358235899727d137df2bc9a4fb221b4f4e3
yagmurlu <mavibulut0106@gmail.com>
2021-11-01 12:23:41 +0300

    WriterRegister Oluşturuldu

---
+++ BlogProject/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            writer.WriterStatus = true;
+            writer.About = "deneme Test";
+            wm.WriterAdd(writer);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
